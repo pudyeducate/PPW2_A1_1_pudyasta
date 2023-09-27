7a6714fe2b188a074131fa395e5fa07b8421bd6c
pudyasta <pudyastasatria@gmail.com>
2023-09-27 15:25:00 +0700

    fix faker

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -8,14 +8,25 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-
+use Faker\Factory as Faker;
 class BarangSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        Barang::factory(10)->create();
+        $faker = Faker::create();
+
+        foreach (range(1, 20) as $index) { 
+            DB::table('barangs')->insert([
+                'nama_barang' => $faker->text(50),
+                'harga' => $faker->numberBetween(10, 1000),
+                'stok' => $faker->numberBetween(1, 100),
+                'id_suplier' => $faker->uuid, 
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
