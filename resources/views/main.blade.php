f7643083cb9967cf40e805e78bdb5f7b06208f54
pudyasta <pudyastasatria@gmail.com>
2023-09-27 15:02:21 +0700

    fix(title)

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -13,7 +13,7 @@
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,600;0,700;0,800;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/css/@yield('css')">
-    <title>Sitani | @yield('title')</title>
+    <title>Barang Gudang | @yield('title')</title>
 </head>
 
 <body>
